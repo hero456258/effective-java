4d72e6be6611cacfa2144135c41a32d8a0738670
wanggang <wanggang@euka.cn>
2019-06-07 11:10:24 +0800

    init project

---
+++ src/main/java/com/mingqian/chapter02/TestMain.java
@@ -0,0 +1,10 @@
+package com.mingqian.chapter02;
+
+/**
+ * Created by wanggang on 2019/6/7.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
